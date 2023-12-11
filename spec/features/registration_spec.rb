7ec81993f0995abac38bfb9c1e3240be941e9823
Anthea Yur <acyur6@gmail.com>
2023-12-11 15:38:50 -0600

    Update testing

--- spec/features/registration_spec.rb
+++ spec/features/registration_spec.rb
@@ -2,14 +2,33 @@ require 'rails_helper'
 
 RSpec.describe "User Registration" do
   it 'can create a user with a name and unique email' do
+    # When I visit `/register`
     visit register_path
+    save_and_open_page
 
-    fill_in :user_name, with: 'User One'
-    fill_in :user_email, with:'user1@example.com'
-    click_button 'Create New User'
+    # I see a form to fill in my name, email, password, and password confirmation.
+    expect(page).to have_field(:username)
+    expect(page).to have_field(:email)
+    expect(page).to have_field(:password)
+    expect(page).to have_field(:password_confirmation)
 
-    expect(current_path).to eq(user_path(User.last.id))
-    expect(page).to have_content("User One's Dashboard")
+    username = "crisPchisP"
+    password = "candycanes"
+
+    # When I fill in that form with my name, email, and matching passwords,
+    fill_in :username, with: username
+    fill_in :email, with: email
+    fill_in :password, with: password
+    fill_in :password_confirmation, with: password
+
+    click_on "Create User"
+
+    # I'm taken to my dashboard page `/users/:id`
+    user = User.last
+    expect(current_path).to eq(user_path(user.id))
+    expect(page).to have_content("Password matches. Welcome, #{username}!")
+    expect(user).to_not have_attribute(:password)
+    expect(user.password_digest).to_not eq(password)
   end 
 
   it 'does not create a user if email isnt unique' do 
