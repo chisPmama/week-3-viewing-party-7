8c0387166df2513c0704ad6d2642283aabb785fa
Anthea Yur <acyur6@gmail.com>
2023-12-11 18:17:48 -0600

    Create testing for log in

---
+++ spec/features/log_in_spec.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+RSpec.describe "User Log In Page" do
+  before(:each) do
+    @user = User.create(name: "ChisP", password: "candycanes", password_confirmation: "candycanes", email: "chisP@gmail.com")
+    @email = "chisP@gmail.com"
+    @password = "candycanes"
+  end
+
+  describe 'Logging In (Happy Path)' do
+    it 'can log into the user account through the landing page' do
+      visit root_path
+
+      click_link "Log In"
+      expect(current_path).to eq(login_path)
+
+      expect(page).to have_field(:email)
+      expect(page).to have_field(:password)
+
+      fill_in :email, with: @email
+      fill_in :password, with: @password
+      click_link "Log In"
+
+      expect(current_path).to eq(user_path(@user))
+    end
+  end
+end
